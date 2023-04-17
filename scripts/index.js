0ef63e708980dfb0b37a7696ab6f209f73e5d90f
benoitDesnos <Benoit.desnos66@gmail.com>
2023-04-17 11:50:21 +0200

    "styling done"

--- scripts/index.js
+++ scripts/index.js
@@ -28,38 +28,53 @@ function recipeFactory(recipe) {
   } = recipe;
 
   const cardInfos = document.createElement("section");
+  cardInfos.classList.add("recipe__wrapper");
+  const cardImgContainer = document.createElement("div");
+  cardImgContainer.classList.add("recipe__image__container");
+  const cardInfosContainer = document.createElement("div");
+  cardInfosContainer.classList.add("recipe__infos__container");
 
   const cardInfosTitle = document.createElement("h3");
+  cardInfosTitle.classList.add("recipe__title");
   cardInfosTitle.textContent = name;
 
-  const icon = document.createElement("img");
-  icon.setAttribute("src", "./../images/time.svg");
-  const cardInfosTimeContainer = document.createElement("span");
+  const cardInfosTimeIcon = document.createElement("img");
+  cardInfosTimeIcon.classList.add("recipe__time__icon");
+  cardInfosTimeIcon.setAttribute("src", "./../images/time.svg");
+
   const cardInfosTime = document.createElement("p");
+  cardInfosTime.classList.add("recipe__time");
   cardInfosTime.textContent = time + " min";
-  cardInfosTimeContainer.appendChild(icon);
+
+  const cardInfosTimeContainer = document.createElement("span");
+  cardInfosTimeContainer.classList.add("recipe__time__container");
+  cardInfosTimeContainer.appendChild(cardInfosTimeIcon);
   cardInfosTimeContainer.appendChild(cardInfosTime);
-  console.log(icon);
 
   const cardInfosRecipe = document.createElement("ul");
+  cardInfosRecipe.classList.add("recipe__ingredients");
+
   let cardInfosingredient;
   ingredients.forEach((recipe) => {
     const { ingredient, quantity, unit } = recipe;
     cardInfosingredient = document.createElement("li");
-    cardInfosingredient.textContent = `${ingredient}${
+    cardInfosingredient.classList.add("recipe__ingredient");
+    cardInfosingredient.innerHTML = `<span><strong>${ingredient}</strong>${
       quantity ? ": " + quantity : ""
-    } ${unit ? unit : ""}`;
+    }${unit ? unit : ""}</span>`;
     cardInfosRecipe.appendChild(cardInfosingredient);
   });
   const cardInfosDescription = document.createElement("p");
+  cardInfosDescription.classList.add("recipe__description");
   cardInfosDescription.textContent = description;
 
   function getRecipeCardDOM() {
-    cardInfos.appendChild(cardInfosTitle);
-
-    cardInfos.appendChild(cardInfosTimeContainer);
-    cardInfos.appendChild(cardInfosRecipe);
-    cardInfos.appendChild(cardInfosDescription);
+    cardInfos.appendChild(cardImgContainer);
+    cardInfos.appendChild(cardInfosContainer);
+    cardInfosContainer.appendChild(cardInfosTitle);
+    cardInfosContainer.appendChild(cardInfosTimeContainer);
+    cardInfosContainer.appendChild(cardInfosRecipe);
+    cardInfosContainer.appendChild(cardInfosDescription);
     return cardInfos;
   }
 
