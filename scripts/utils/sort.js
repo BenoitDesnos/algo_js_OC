c8c4bfcf1e8ff115f3da8cdcea2a91fa14f58541
benoitDesnos <Benoit.desnos66@gmail.com>
2023-04-24 12:23:34 +0200

    sort by ingredients and description ok

--- scripts/utils/sort.js
+++ scripts/utils/sort.js
@@ -1,30 +1,32 @@
 const searchBar = document.getElementById("search__bar");
-let searchedString;
-let titleString;
-let ingredientString = [];
-let utensilString;
-
-console.log(searchBar);
 
 searchBar.addEventListener("input", (e) => {
   /*  const recipeTitles = document.querySelectorAll(".recipe__title"); */
   const recipeInfosContainer = document.querySelectorAll(
     ".recipe__infos__container"
   );
-  searchedString = e.target.value;
+  let searchedString = e.target.value;
 
-  recipeInfosContainer.forEach((container) => {
-    titleString = container.children[0].textContent;
+  recipeInfosContainer.forEach((container) => {       
+    let titleString = container.querySelector('.recipe__title').textContent;    
+    let descriptionString = container.querySelector('.recipe__description').textContent;    
+    let ingredientsString = returnIngredientsByContainer(container)   
     if (
       !makeStringCaseAndAccentInsensitive(titleString).includes(
         makeStringCaseAndAccentInsensitive(searchedString)
+      ) && !makeStringCaseAndAccentInsensitive(ingredientsString).includes(
+        makeStringCaseAndAccentInsensitive(searchedString)
+      ) &&
+       !makeStringCaseAndAccentInsensitive(descriptionString).includes(
+        makeStringCaseAndAccentInsensitive(searchedString)
       ) &&
       e.target.value.length >= 3
-    ) {
-      console.log("test");
+    ) {      
       container.closest(".recipe__wrapper").style.display = "none";
+      
     } else {
       container.closest(".recipe__wrapper").style.display = "block";
+      
     }
   });
 });
@@ -34,4 +36,18 @@ function makeStringCaseAndAccentInsensitive(prop) {
     .toLowerCase()
     .normalize("NFD")
     .replace(/[\u0300-\u036f]/g, "");
+}
+
+function returnIngredientsByContainer(container) {
+  let ingredientString = "";
+  let result = "" 
+  console.log('--------------');
+  ingredients = container.querySelectorAll('.ingredient');
+  console.log(ingredients);
+    ingredients.forEach((ingredient) => {
+      result += ingredientString.concat(ingredient.textContent, " ")
+     
+    })
+    
+    return result
 }
